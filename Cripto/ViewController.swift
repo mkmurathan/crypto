765f23382535ebaed3477fd7ef78370191793722
Murathan Karagöz <murathankaragooz@hotmail.com>
2023-08-22 00:59:15 +0300

    Initial Commit

---
+++ Cripto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cripto
+//
+//  Created by Murathan karagöz on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
